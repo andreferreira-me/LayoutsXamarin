f782a817e6fa0c854ced34c9e98a790ac3f1bbfe
André Ferreira <contato@andreferreira.me>
2017-03-08 01:42:15 -0300

    First commit

--- Layouts/Resources/Resource.Designer.cs
+++ Layouts/Resources/Resource.Designer.cs
@@ -57,11 +57,60 @@ namespace Layouts
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050005
+			public const int button1 = 2131034117;
+			
+			// aapt resource value: 0x7f050006
+			public const int button2 = 2131034118;
+			
+			// aapt resource value: 0x7f050007
+			public const int button3 = 2131034119;
+			
+			// aapt resource value: 0x7f050001
+			public const int radioButton1 = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int radioButton2 = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int radioButton3 = 2131034115;
+			
+			// aapt resource value: 0x7f050000
+			public const int radioGroup1 = 2131034112;
+			
+			// aapt resource value: 0x7f050004
+			public const int relativeLayout1 = 2131034116;
+			
+			// aapt resource value: 0x7f050008
+			public const int textview1 = 2131034120;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int LinearLayout = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Main = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int RelativeLayout = 2130903042;
+			
+			// aapt resource value: 0x7f030003
+			public const int TableLayout = 2130903043;
 			
 			static Layout()
 			{
